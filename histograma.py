e2dc729f6f5cf858357314e878c142961a42daa2
Zikofv <zikofv@gmail.com>
2013-05-06 21:07:36 -0300

    Elimino lineas que no se usan en histograma.py

--- histograma.py
+++ histograma.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import colorsys
 import sys
-from collections import defaultdict
 
 def cargar(filename):
   img = ImagenArchivo(filename)
@@ -16,14 +15,6 @@ def cargar(filename):
 def crear_histograma(img):
   ancho, alto = img.size
 
-  valores_h = defaultdict(int)
-  valores_s = defaultdict(int)
-  valores_v = defaultdict(int)
-  indices_h = set()
-  indices_s = set()
-  indices_v = set()
-  total_pixels = 0
-
   histo = []
 
   for x in range(ancho):
@@ -32,10 +23,6 @@ def crear_histograma(img):
       if not (r == 0 and g == 0 and b == 0):
         h, s, v = colorsys.rgb_to_hsv(r/255, g/255, b/255)
         histo.append(h)
-        #valores_h[int(h)] += 1
-        #valores_h[s] += 1
-        #valores_h[v] += 1
-        #total_pixels += 1
 
   plt.hist(histo, bins=np.arange(0., 1., 0.001))
   plt.show()
@@ -43,21 +30,3 @@ def crear_histograma(img):
 if __name__ == '__main__':
   img = cargar(sys.argv[1])
   crear_histograma(img)
-
-
-#Codigo viejo para mostrar el histograma
-"""
-indices = []
-alturas = []
-for i in valores_h.iteritems():
-  indices.append(i[0])
-  alturas.append(i[1] / total_pixels)
-
-
-width = 0.001 #the width of the bars: can also be len(x) sequence
-p1 = plt.bar(indices, alturas, width, color='r', align="center")
-p2 = plt.bar(indices, alturas, width, color='b', align="center")
-plt.ylabel('intensidad')
-plt.xlabel('hue')
-plt.show()
-"""
