ef2de32adf8952eb15cd14013b7e9c4ce396361b
Zikofv <zikofv@gmail.com>
2013-06-05 11:10:56 -0300

    Borro algunas cosas de mas en imageviewer.py

--- imageviewer.py
+++ imageviewer.py
@@ -1,52 +1,23 @@
 #!/usr/bin/env python
 
-#############################################################################
-##
-## Copyright (C) 2005-2005 Trolltech AS. All rights reserved.
-##
-## This file is part of the example classes of the Qt Toolkit.
-##
-## This file may be used under the terms of the GNU General Public
-## License version 2.0 as published by the Free Software Foundation
-## and appearing in the file LICENSE.GPL included in the packaging of
-## this file.  Please review the following information to ensure GNU
-## General Public Licensing requirements will be met:
-## http://www.trolltech.com/products/qt/opensource.html
-##
-## If you are unsure which license is appropriate for your use, please
-## review the following information:
-## http://www.trolltech.com/products/qt/licensing.html or contact the
-## sales department at sales@trolltech.com.
-##
-## This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
-## WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-##
-#############################################################################
-
 from PySide import QtCore, QtGui
 from collections import namedtuple
 from transformedWidget import *
 
 import sys
 import cv2 as cv
-import numpy
 import warp_image as wi
-
-
-class Communicate(QtCore.QObject):
-    
-    mouseClick = QtCore.Signal()
 
 class ImageViewer(QtGui.QMainWindow):
     WIDHT_DEST = 600
     HEIGHT_DEST = 600
     points = {}
-    points_dest = {0: (0,0), 1: (WIDHT_DEST,0), 2: (WIDHT_DEST,HEIGHT_DEST), 3: (0,HEIGHT_DEST)} 
+    points_dest = {0: (0,0), 1: (WIDHT_DEST,0), 2: (WIDHT_DEST,HEIGHT_DEST), 3: (0,HEIGHT_DEST)}
     i = 0
+
     def __init__(self, filename):
         super(ImageViewer, self).__init__()
 
-        self.printer = QtGui.QPrinter()
         self.scaleFactor = 1.0
 
         self.imageLabel = LabelImage()
@@ -70,17 +41,15 @@ class ImageViewer(QtGui.QMainWindow):
 
         if filename:
             self.loadImage(filename)
-            
+
             self.scaleFactor = 1.0
 
-            self.printAct.setEnabled(True)
             self.fitToWindowAct.setEnabled(True)
             self.updateActions()
 
             if not self.fitToWindowAct.isChecked():
                 self.imageLabel.adjustSize()
 
-        self.printAct.setEnabled(True)
         self.fitToWindowAct.setEnabled(True)
         self.updateActions()
 
@@ -105,23 +74,11 @@ class ImageViewer(QtGui.QMainWindow):
 
         self.scaleFactor = 1.0
 
-        self.printAct.setEnabled(True)
         self.fitToWindowAct.setEnabled(True)
         self.updateActions()
 
         if not self.fitToWindowAct.isChecked():
             self.imageLabel.adjustSize()
-
-    def print_(self):
-        dialog = QtGui.QPrintDialog(self.printer, self)
-        if dialog.exec_():
-            painter = QtGui.QPainter(self.printer)
-            rect = painter.viewport()
-            size = self.imageLabel.pixmap().size()
-            size.scale(rect.size(), QtCore.Qt.KeepAspectRatio)
-            painter.setViewport(rect.x(), rect.y(), size.width(), size.height())
-            painter.setWindow(self.imageLabel.pixmap().rect())
-            painter.drawPixmap(0, 0, self.imageLabel.pixmap())
 
     def zoomIn(self):
         self.scaleImage(1.25)
@@ -140,24 +97,8 @@ class ImageViewer(QtGui.QMainWindow):
             self.normalSize()
         self.updateActions()
 
-    def about(self):
-        QtGui.QMessageBox.about(self, "About Image Viewer",
-                "<p>The <b>Image Viewer</b> example shows how to combine "
-                "QLabel and QScrollArea to display an image. QLabel is "
-                "typically used for displaying text, but it can also display "
-                "an image. QScrollArea provides a scrolling view around "
-                "another widget. If the child widget exceeds the size of the "
-                "frame, QScrollArea automatically provides scroll bars.</p>"
-                "<p>The example demonstrates how QLabel's ability to scale "
-                "its contents (QLabel.scaledContents), and QScrollArea's "
-                "ability to automatically resize its contents "
-                "(QScrollArea.widgetResizable), can be used to implement "
-                "zooming and scaling features.</p>"
-                "<p>In addition the example shows how to use QPainter to "
-                "print an image.</p>")
-
     def transform(self):
-        cv_dest = wi.warpImage(self.cv_img, self.dictToList(self.points), self.dictToList(self.points_dest), self.WIDHT_DEST, self.HEIGHT_DEST) 
+        cv_dest = wi.warpImage(self.cv_img, self.dictToList(self.points), self.dictToList(self.points_dest), self.WIDHT_DEST, self.HEIGHT_DEST)
 
         if hasattr(self,'transWidget'):
             self.transObjet.setImage(self.openCVtoQImage(cv_dest))
@@ -172,9 +113,6 @@ class ImageViewer(QtGui.QMainWindow):
     def createActions(self):
         self.openAct = QtGui.QAction("&Open...", self, shortcut="Ctrl+O",
                 triggered=self.open)
-
-        self.printAct = QtGui.QAction("&Print...", self, shortcut="Ctrl+P",
-                enabled=False, triggered=self.print_)
 
         self.exitAct = QtGui.QAction("E&xit", self, shortcut="Ctrl+Q",
                 triggered=self.close)
@@ -192,11 +130,6 @@ class ImageViewer(QtGui.QMainWindow):
                 enabled=False, checkable=True, shortcut="Ctrl+F",
                 triggered=self.fitToWindow)
 
-        self.aboutAct = QtGui.QAction("&About", self, triggered=self.about)
-
-        self.aboutQtAct = QtGui.QAction("About &Qt", self,
-                triggered=QtGui.qApp.aboutQt)
-
         self.transformAct = QtGui.QAction("&Transform", self,
                 shortcut="Ctrl+T", enabled=False, triggered=self.transform)
 
@@ -209,7 +142,6 @@ class ImageViewer(QtGui.QMainWindow):
     def createMenus(self):
         self.fileMenu = QtGui.QMenu("&File", self)
         self.fileMenu.addAction(self.openAct)
-        self.fileMenu.addAction(self.printAct)
         self.fileMenu.addSeparator()
         self.fileMenu.addAction(self.exitAct)
 
@@ -220,16 +152,11 @@ class ImageViewer(QtGui.QMainWindow):
         self.viewMenu.addSeparator()
         self.viewMenu.addAction(self.fitToWindowAct)
 
-        self.helpMenu = QtGui.QMenu("&Help", self)
-        self.helpMenu.addAction(self.aboutAct)
-        self.helpMenu.addAction(self.aboutQtAct)
-
         self.transformMenu = QtGui.QMenu("&Transform", self)
         self.transformMenu.addAction(self.transformAct)
 
         self.menuBar().addMenu(self.fileMenu)
         self.menuBar().addMenu(self.viewMenu)
-        self.menuBar().addMenu(self.helpMenu)
         self.menuBar().addMenu(self.transformMenu)
 
     def updateActions(self):
@@ -263,7 +190,7 @@ class ImageViewer(QtGui.QMainWindow):
             self.update()
 
         self.i += 1
-	
+
         self.transformAct.setEnabled(self.i == 4)
 
 
@@ -278,7 +205,6 @@ class ImageViewer(QtGui.QMainWindow):
                 painter.drawLine(self.points[self.i-1][0], self.points[self.i-1][1], self.points[0][0], self.points[0][1])
             painter.end()
             self.imageLabel.setPixmap(QtGui.QPixmap.fromImage(self.imageLabel.pixmap().toImage()))
-
 
     def openCVtoQImage(self, img_cv):
 
@@ -297,7 +223,7 @@ class LabelImage(QtGui.QLabel):
         super(LabelImage, self).__init__()
 
     def mousePressEvent(self, event):
-	self.imageClicked.emit(event.x(),event.y())
+      self.imageClicked.emit(event.x(),event.y())
 
 if __name__ == '__main__':
 
